caa1614f513a22abb8fb1678c85d00b85bd4bf2b
Pedro Moura <pedro.moura@celfinet.com>
2017-10-21 14:46:51 +0100

    add eslint plugins and config to project

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
- 	<div><h1>Teste</h1></div>,
+	<div><h1>Teste</h1></div>,
 	document.getElementById('app')
 );
--- webpack.common.js
+++ webpack.common.js
@@ -4,32 +4,32 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = {
-	entry: {
+    entry: {
         app: './src/index.js',
     },
     module: {
         rules: [
             { test: /\.(js|jsx)$/, exclude: /node_modules/, loaders: ["babel-loader"] },
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
             { test: /\.(png|svg|jpg|gif)$/, use: ['file-loader'] },
-            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },         
+            { test: /\.(woff|woff2|eot|ttf|otf)$/, use: ['file-loader'] },
         ],
     },
-	plugins: [
-		new CleanWebpackPlugin(['dist']),
-		new HtmlWebpackPlugin({
-			 title: 'JSChanllenge',
-			 filename: 'index.html',
-			 template: './src/assets/index.html'			 
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'JSChanllenge',
+            filename: 'index.html',
+            template: './src/assets/index.html'
         }),
         new webpack.optimize.CommonsChunkPlugin({
-            name: "commons",          
-            filename: "commons.js",          
+            name: "commons",
+            filename: "commons.js",
             minChunks: 2,
         })
-  	],
-  	output: {
-		filename: '[name].js',
-		path: path.resolve(__dirname, 'dist')
-  	},
+    ],
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
 };
